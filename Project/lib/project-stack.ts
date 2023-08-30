87e07f9f529d2373dfedad7f7752f611f45b0b90
Micha de Jong <115347170+Mynamewastakenwastaken@users.noreply.github.com>
2023-08-30 13:54:37 +0200

    Update project-stack.ts

--- Project/lib/project-stack.ts
+++ Project/lib/project-stack.ts
@@ -1,9 +1,24 @@
 import * as cdk from 'aws-cdk-lib';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
 
 export class ProjectStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // nothing here!
+    const vpc = new ec2.Vpc(this, 'ProductionVPC', {
+      maxAzs: 2,  // We're using 2 AZ's
+      subnetConfiguration: [  // Configuration for the subnets
+        {
+          cidrMask: 24,  // The CIDR notation for a subnet (optional)
+          name: 'ProductionPublic',
+          subnetType: ec2.SubnetType.PUBLIC,  // Indicates this is a public subnet
+        },
+        {
+          cidrMask: 24,  // The CIDR notation for a subnet (optional)
+          name: 'ProductionPrivate',
+          subnetType: ec2.SubnetType.PRIVATE_ISOLATED,  // Indicates this is a private subnet
+        },
+      ],
+    });
   }
 }
