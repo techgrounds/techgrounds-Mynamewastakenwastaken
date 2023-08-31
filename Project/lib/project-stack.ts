9169c3494ed175675397e2f8e1ca773a4d43d03f
Micha de Jong <115347170+Mynamewastakenwastaken@users.noreply.github.com>
2023-08-31 11:18:37 +0200

    Update project-stack.ts

--- Project/lib/project-stack.ts
+++ Project/lib/project-stack.ts
@@ -28,8 +28,8 @@ export class ProjectStack extends cdk.Stack {
     });
 
     const VPCPeeringConnection = new ec2.CfnVPCPeeringConnection(this, 'Production_Admin_Peering', {
-      peerVpcId: vpc.node.addr,
-      vpcId: vpc2.node.addr,
+      peerVpcId: vpc.vpcArn,
+      vpcId: vpc2.vpcArn,
     });
   }
 }
