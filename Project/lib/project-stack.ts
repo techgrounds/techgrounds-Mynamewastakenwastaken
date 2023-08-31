fb5af26824defa2a0e312b0badd23b9e72fbe012
Micha de Jong <115347170+Mynamewastakenwastaken@users.noreply.github.com>
2023-08-31 15:00:18 +0200

    IAM webserver role

--- Project/lib/project-stack.ts
+++ Project/lib/project-stack.ts
@@ -1,32 +1,36 @@
 import * as cdk from 'aws-cdk-lib';
 import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import * as iam from 'aws-cdk-lib/aws-iam';
 
 export class ProjectStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+      // Create the Production VPC
     const vpc = new ec2.Vpc(this, 'ProductionVPC', {
       ipAddresses: ec2.IpAddresses.cidr('10.10.10.0/24'),
-      maxAzs: 2,  // We're using 2 AZ's
-      subnetConfiguration: [  // Configuration for the subnets
+      maxAzs: 2,
+      subnetConfiguration: [
         {
           name: 'ProductionPublic',
-          subnetType: ec2.SubnetType.PUBLIC,  // Public subnet
+          subnetType: ec2.SubnetType.PUBLIC,
         },
       ],
     });
 
+      // create the Admin VPC
     const vpc2 = new ec2.Vpc(this, 'AdminVPC', {
       ipAddresses: ec2.IpAddresses.cidr('10.20.20.0/24'),
-      maxAzs: 2,  // We're using 2 AZ's
-      subnetConfiguration: [  // Configuration for the subnets
+      maxAzs: 2,
+      subnetConfiguration: [
         {
           name: 'AdminPublic',
-          subnetType: ec2.SubnetType.PUBLIC,  // Public subnet
+          subnetType: ec2.SubnetType.PUBLIC,
         },
       ],
     });
 
+      // Create the peering connection
     const VPCPeeringConnection = new ec2.CfnVPCPeeringConnection(this, 'Production_Admin_Peering', {
       peerVpcId: vpc.vpcId,
       vpcId: vpc2.vpcId,
@@ -50,17 +54,23 @@ export class ProjectStack extends cdk.Stack {
       });
     });
 
-      // Create a security group for your instances
+      // Create a security group for inbound rules
     const ProductionSG = new ec2.SecurityGroup(this, 'AdminAccess', {
         vpc,
       });
 
-      // Add an ingress rule to allow SSH traffic only from 10.20.20.0/24
+      // Add an inbound rule to allow SSH traffic from 10.20.20.0/24
     ProductionSG.addIngressRule(ec2.Peer.ipv4('10.20.20.0/24'), ec2.Port.tcp(22), 'Allow SSH from 10.20.20.0/24');
 
-      // Add an ingress rule to allow RDP traffic only from 10.20.20.0/24
+      // Add an inbound rule to allow RDP traffic from 10.20.20.0/24
     ProductionSG.addIngressRule(ec2.Peer.ipv4('10.20.20.0/24'), ec2.Port.tcp(3389), 'Allow RDP from 10.20.20.0/24');
 
+    const instanceRole = new iam.Role(this, 'InstanceRole', {
+      assumedBy: new iam.ServicePrincipal('ec2.amazonaws.com'),
+      roleName: 'Webserver',
+    });
+    
+    instanceRole.addManagedPolicy(iam.ManagedPolicy.fromAwsManagedPolicyName('AmazonEC2FullAccess'));
 
   }
 }
