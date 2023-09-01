cd0adbd1c2c173be6169b7ec6a940828317e94e0
Micha de Jong <115347170+Mynamewastakenwastaken@users.noreply.github.com>
2023-09-01 10:26:54 +0200

    Admin SG & EBS for Production

--- Project/lib/project-stack.ts
+++ Project/lib/project-stack.ts
@@ -2,6 +2,8 @@ import * as cdk from 'aws-cdk-lib';
 import * as ec2 from 'aws-cdk-lib/aws-ec2';
 import * as iam from 'aws-cdk-lib/aws-iam';
 import {readFileSync} from 'fs';
+import * as kms from 'aws-cdk-lib/aws-kms';
+
 
 export class ProjectStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
@@ -55,9 +57,10 @@ export class ProjectStack extends cdk.Stack {
       });
     });
 
-      // Create a security group for inbound rules
-    const ProductionSG = new ec2.SecurityGroup(this, 'AdminAccess', {
-        vpc,
+      // Create a security group for Production
+    const ProductionSG = new ec2.SecurityGroup(this, 'ProductionAccess', {
+        vpc: vpc,
+        description: 'Allow HTTP access and Admin access'
       });
 
       // Add an inbound rule to allow SSH traffic from 10.20.20.0/24
@@ -70,24 +73,65 @@ export class ProjectStack extends cdk.Stack {
     ProductionSG.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(80), 'Allow HTTP traffic');
     ProductionSG.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(443), 'Allow HTTPS traffic');
 
-    //   // Create role for the webserver instance
-    // const instanceRole = new iam.Role(this, 'WebRole', {
-    //   assumedBy: new iam.ServicePrincipal('ec2.amazonaws.com'),
-    //   roleName: 'WebserverRole',
-    // });
+      // Create a security group for Admin
+    const AdminSG = new ec2.SecurityGroup(this, 'AdminAccess', {
+        vpc: vpc2,
+        description: 'Allow public access from select ip'
+      });    
 
-    // instanceRole.addManagedPolicy(iam.ManagedPolicy.fromAwsManagedPolicyName('AmazonEC2FullAccess'));
+      // Add an inbound rule to allow SSH traffic from 10.20.20.0/24
+    AdminSG.addIngressRule(ec2.Peer.ipv4('80.112.80.150'), ec2.Port.tcp(22), 'Allow SSH from 80.112.80.150');
 
-    // const instance = new ec2.Instance(this, 'Webserver', {
-    //   instanceType: ec2.InstanceType.of(ec2.InstanceClass.T2, ec2.InstanceSize.MICRO),
-    //   machineImage: ec2.MachineImage.latestAmazonLinux2(),
-    //   vpc,
-    //   securityGroup: ProductionSG,
-    //   role: instanceRole,
-    // });
+      // Add an inbound rule to allow SSH traffic from 10.20.20.0/24
+    ProductionSG.addIngressRule(ec2.Peer.ipv4('10.20.20.0/24'), ec2.Port.tcp(22), 'Allow SSH from 10.20.20.0/24');
+
+      // Create role for the production instance
+    const instanceRole = new iam.Role(this, 'Instance', {
+      assumedBy: new iam.ServicePrincipal('ec2.amazonaws.com'),
+      roleName: 'InstanceRole',
+    });
+
+    instanceRole.addManagedPolicy(iam.ManagedPolicy.fromAwsManagedPolicyName('AmazonEC2FullAccess'));
+
+      // Create production instance
+    const instance = new ec2.Instance(this, 'Webserver', {
+      instanceType: ec2.InstanceType.of(ec2.InstanceClass.T2, ec2.InstanceSize.MICRO),
+      machineImage: ec2.MachineImage.latestAmazonLinux2(),
+      vpc: vpc,
+      securityGroup: ProductionSG,
+      role: instanceRole,
+    });
     
-    // const userDataScript = readFileSync('./lib/userdata.sh', 'utf8');
-    // instance.addUserData(userDataScript);
+      // Create admin instance
+    const instance2 = new ec2.Instance(this, 'Admninserver', {
+      instanceType: ec2.InstanceType.of(ec2.InstanceClass.T2, ec2.InstanceSize.MICRO),
+      machineImage: ec2.MachineImage.latestAmazonLinux2(),
+      vpc: vpc2,
+      securityGroup: AdminSG,        
+      role: instanceRole,
+    });
+
+    const userDataScript = readFileSync('./lib/userdata.sh', 'utf8');
+    instance.addUserData(userDataScript);
+    instance2.addUserData(userDataScript);
+
+      // default = general purpose SSD
+    const volume = new ec2.Volume(this, 'ProductionEBS', {
+      availabilityZone: 'eu-central-1a',
+      size: Size.gibibytes(1),
+      encrypted: true,
+    });
+    
+    volume.grantAttachVolume(instanceRole, [instance]);
+
+
+    // const encryptionKey = new kms.Key(this, 'ProductionKey', {
+    //   enableKeyRotation: true,
+    // });
+
+    // const encryptionKey2 = new kms.Key(this, 'AdminKey', {
+    //   enableKeyRotation: true,
+    // });
 
   }
 }
