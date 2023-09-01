71c12925acca4fdd24b791428c2a0edd83e5d0de
Micha de Jong <115347170+Mynamewastakenwastaken@users.noreply.github.com>
2023-09-01 14:50:26 +0200

    admin server -> windows

--- Project/lib/project-stack.ts
+++ Project/lib/project-stack.ts
@@ -109,7 +109,7 @@ export class ProjectStack extends cdk.Stack {
       // Create admin instance
     const instance2 = new ec2.Instance(this, 'Admninserver', {
       instanceType: ec2.InstanceType.of(ec2.InstanceClass.T2, ec2.InstanceSize.MICRO),
-      machineImage: ec2.MachineImage.latestAmazonLinux2(),
+      machineImage: ec2.MachineImage.latestWindows(ec2.WindowsVersion.WINDOWS_SERVER_2019_ENGLISH_FULL_BASE),
       vpc: vpc2,
       securityGroup: AdminSG,        
       role: instanceRole2,
