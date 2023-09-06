d346daa7c00a7d0bd2d8d319558600d1c5a490c2
Micha de Jong <115347170+Mynamewastakenwastaken@users.noreply.github.com>
2023-09-06 13:07:32 +0200

    Update userdata-selfsigned.sh

---
+++ Project/lib/userdata-selfsigned.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+yum -y install httpd
+systemctl enable httpd
+systemctl start httpd
+echo '<html><h1>Hello From Your Web Server!</h1></html>' > /var/www/html/index.html
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/pki/tls/private/selfsigned.key -out /etc/pki/tls/certs/selfsigned.crt
+cat <<EOF > /etc/httpd/conf.d/selfsigned.conf
+<VirtualHost *:443>
+    ServerAdmin webmaster@localhost
+    DocumentRoot /var/www/html
+    SSLEngine on
+    SSLCertificateFile /etc/pki/tls/certs/selfsigned.crt
+    SSLCertificateKeyFile /etc/pki/tls/private/selfsigned.key
+</VirtualHost>
+EOF
+systemctl restart httpd
