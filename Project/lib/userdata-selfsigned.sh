bb701d8617df7a0bc8caf16a9e940d33a8d0cb49
Micha de Jong <115347170+Mynamewastakenwastaken@users.noreply.github.com>
2023-09-06 13:46:03 +0200

    Update userdata-selfsigned.sh

--- Project/lib/userdata-selfsigned.sh
+++ Project/lib/userdata-selfsigned.sh
@@ -2,7 +2,14 @@ yum -y install httpd
 systemctl enable httpd
 systemctl start httpd
 echo '<html><h1>Hello From Your Web Server!</h1></html>' > /var/www/html/index.html
-openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/pki/tls/private/selfsigned.key -out /etc/pki/tls/certs/selfsigned.crt
+openssl req -newkey rsa:4096 \
+            -x509 \
+            -sha256 \
+            -days 3650 \
+            -nodes \
+            -out selfsigned.crt \
+            -keyout selfsigned.key \
+            -subj "/C=SI/ST=Ljubljana/L=Ljubljana/O=Security/OU=IT Department/CN=www.example.com"
 cat <<EOF > /etc/httpd/conf.d/selfsigned.conf
 <VirtualHost *:443>
     ServerAdmin webmaster@localhost
