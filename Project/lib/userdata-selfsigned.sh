201742d656c214453094baa738a5a13c1af62c0d
Micha de Jong <115347170+Mynamewastakenwastaken@users.noreply.github.com>
2023-09-07 09:26:07 +0200

    Update userdata-selfsigned.sh

--- Project/lib/userdata-selfsigned.sh
+++ Project/lib/userdata-selfsigned.sh
@@ -2,21 +2,14 @@ yum -y install httpd
 systemctl enable httpd
 systemctl start httpd
 echo '<html><h1>Hello From Your Web Server!</h1></html>' > /var/www/html/index.html
-openssl req -newkey rsa:4096 \
-            -x509 \
-            -sha256 \
-            -days 3650 \
-            -nodes \
-            -out selfsigned.crt \
-            -keyout selfsigned.key \
-            -subj "/C=SI/ST=Ljubljana/L=Ljubljana/O=Security/OU=IT Department/CN=www.example.com"
-cat <<EOF > /etc/httpd/conf.d/selfsigned.conf
+openssl req -newkey rsa:4096 -x509 -sha256 -days 3650 -nodes -out /etc/ssl/certs/selfsigned.crt -keyout /etc/ssl/certs/selfsigned.key -subj "/C=SI/ST=Ljubljana/L=Ljubljana/O=Security/OU=IT Department/CN=www.example.com"
+cat <<EOF > /etc/httpd/conf/apachessl.conf
 <VirtualHost *:443>
     ServerAdmin webmaster@localhost
     DocumentRoot /var/www/html
     SSLEngine on
-    SSLCertificateFile /etc/pki/tls/certs/selfsigned.crt
-    SSLCertificateKeyFile /etc/pki/tls/private/selfsigned.key
+    SSLCertificateFile /etc/ssl/certs/selfsigned.crt
+    SSLCertificateKeyFile /etc/ssl/certs/selfsigned.key
 </VirtualHost>
 EOF
 systemctl restart httpd
