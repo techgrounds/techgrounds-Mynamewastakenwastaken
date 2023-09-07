aee9900fa1c1b29ca67358739a5f3b313a453598
Micha de Jong <115347170+Mynamewastakenwastaken@users.noreply.github.com>
2023-09-07 09:52:14 +0200

    working SSL

--- Project/lib/userdata-test.sh
+++
@@ -1,2 +0,0 @@
-yum install httpd mod_ssl -y
-systemctl enable httpd && systemctl start httpd
--- Project/lib/userdata.sh
+++ Project/lib/userdata.sh
@@ -1,4 +1,2 @@
-yum -y install httpd
-systemctl enable httpd
-systemctl start httpd
-echo '<html><h1>Hello From Your Web Server!</h1></html>' > /var/www/html/index.html
+yum install httpd mod_ssl -y
+systemctl enable httpd && systemctl start httpd
