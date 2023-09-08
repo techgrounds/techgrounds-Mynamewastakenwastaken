e5f9a13c6ab9dcb8bf03cf05268141f1974871d4
Micha de Jong <115347170+Mynamewastakenwastaken@users.noreply.github.com>
2023-09-08 16:29:32 +0200

    certificate stack

---
+++ Project/lib/certificate.ts
@@ -0,0 +1,12 @@
+import * as cdk from 'aws-cdk-lib';
+
+export class ProjectStack extends cdk.Stack {
+    constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
+      super(scope, id, props);
+
+
+
+
+      
+    }
+}
--- Project/lib/userdata.sh
+++ Project/lib/userdata.sh
@@ -1,2 +1,3 @@
 yum install httpd mod_ssl -y
+echo Empty | cat >> /var/www/html/index.html
 systemctl enable httpd && systemctl start httpd
