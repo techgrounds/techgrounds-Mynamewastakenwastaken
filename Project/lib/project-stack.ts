356ee8f4f7a94c145a70adf6d12e1f16267fbe3e
Micha de Jong <115347170+Mynamewastakenwastaken@users.noreply.github.com>
2023-09-21 16:06:53 +0200

    Update project-stack.ts

--- Project/lib/project-stack.ts
+++ Project/lib/project-stack.ts
@@ -145,6 +145,7 @@ export class ProjectStack extends cdk.Stack {
       instanceType: ec2.InstanceType.of(ec2.InstanceClass.T2, ec2.InstanceSize.MICRO),
       machineImage: ec2.MachineImage.latestWindows(ec2.WindowsVersion.WINDOWS_SERVER_2019_ENGLISH_FULL_BASE),
       vpc: vpc2,
+      vpcSubnets: { subnetType: ec2.SubnetType.PUBLIC },
       securityGroup: AdminSG,        
       role: instanceRole2,
       keyName: 'AdminKey',
