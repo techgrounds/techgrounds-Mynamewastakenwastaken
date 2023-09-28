c213dea580e4a88b693840f95ea7ada8e9019d6c
Micha de Jong <115347170+Mynamewastakenwastaken@users.noreply.github.com>
2023-09-28 16:41:41 +0200

    Update userdata.sh

--- Project/lib/userdata.sh
+++ Project/lib/userdata.sh
@@ -1,5 +1,6 @@
 yum install httpd mod_ssl -y
-yum install stress -y
+sudo amazon-linux-extras install epel -y
+sudo yum install stress -y
 echo Funny comment | cat >> /var/www/html/index.html
 systemctl enable httpd && systemctl start httpd
 sudo stress --cpu 1 --timeout 600
