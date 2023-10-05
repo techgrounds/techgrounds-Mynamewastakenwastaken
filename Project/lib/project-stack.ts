21c761b990cda32d94fe16a5c4cfccc78955bfa3
Micha de Jong <115347170+Mynamewastakenwastaken@users.noreply.github.com>
2023-10-05 11:31:22 +0200

    Update project-stack.ts

--- Project/lib/project-stack.ts
+++ Project/lib/project-stack.ts
@@ -111,7 +111,8 @@ export class ProjectStack extends cdk.Stack {
     ProductionSG.addIngressRule(ec2.Peer.ipv4('10.20.20.0/24'), ec2.Port.tcp(3389), 'Allow RDP from 10.20.20.0/24');
     // ProductionSG.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(443), 'Allow inbound HTTPS traffic');
     // ProductionSG.addEgressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(443), 'Allow outbound HTTPS traffic');
-    // ProductionSG.addEgressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(80), 'Allow outbound HTTP traffic');
+    ProductionSG.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(80), 'Allow inbound HTTP traffic');
+    ProductionSG.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(443), 'Allow inbound HTTP traffic');
     
     
     //   // !!!!! TESTING REMOVE IN FINAL !!!!!
