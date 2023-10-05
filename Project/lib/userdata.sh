a1482c37b4f285a5199b34408ff46372cbf5f590
Micha de Jong <115347170+Mynamewastakenwastaken@users.noreply.github.com>
2023-10-05 11:36:39 +0200

    Update userdata.sh

--- Project/lib/userdata.sh
+++ Project/lib/userdata.sh
@@ -3,4 +3,4 @@ sudo amazon-linux-extras install epel -y
 sudo yum install stress -y
 echo "<h1>Hello World from $(hostname -f)</h1>" > /var/www/html/index.html
 systemctl enable httpd && systemctl start httpd
-# sudo stress --cpu 4 --timeout 800
+sudo stress --cpu 4 --timeout 800
