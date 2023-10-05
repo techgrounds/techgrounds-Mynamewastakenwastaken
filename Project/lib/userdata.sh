0310ada3c7e06a6e7291ce3d226bbd6e814cb0da
Micha de Jong <115347170+Mynamewastakenwastaken@users.noreply.github.com>
2023-10-05 10:23:19 +0200

    final testing

--- Project/lib/userdata.sh
+++ Project/lib/userdata.sh
@@ -1,6 +1,6 @@
 yum install httpd mod_ssl -y
 sudo amazon-linux-extras install epel -y
 sudo yum install stress -y
-echo Funny comment | cat >> /var/www/html/index.html
+echo "<h1>Hello World from $(hostname -f)</h1>" > /var/www/html/index.html
 systemctl enable httpd && systemctl start httpd
-sudo stress --cpu 4 --timeout 800
+# sudo stress --cpu 4 --timeout 800
